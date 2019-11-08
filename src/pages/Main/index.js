f246ec229dff2012384ac136f63d01b32f105d18
Luis Geniole <luis.geniole@gmail.com>
2019-11-08 06:34:05 -0300

    template repo

---
+++ src/pages/Main/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Container from '../../components/Container';
+
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>Lets Get crackin</h1>
+      </Container>
+    );
+  }
+}
