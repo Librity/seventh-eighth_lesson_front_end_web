bf6abce4d99587699af91020459f6aca2d7bfa8a
Luis Geniole <luis.geniole@gmail.com>
2019-11-12 23:09:19 -0300

    refactor Home w/Hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
@@ -10,59 +10,52 @@ import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [],
+function Home({ amount, addToCartRequest }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const response = await api.get('products');
+
+      const data = response.data.map(product => ({
+        ...product,
+        formattedPrice: formatPricePtBr(product.price),
+      }));
+
+      setProducts(data);
     };
-  }
 
-  async componentDidMount() {
-    const response = await api.get('products');
+    loadProducts();
+  }, []);
 
-    const data = response.data.map(product => ({
-      ...product,
-      formattedPrice: formatPricePtBr(product.price),
-    }));
-
-    this.setState({ products: data });
-  }
-
-  handleAddProduct = id => {
-    const { addToCartRequest } = this.props;
-
+  /**
+   * Since this function doesn't depend on any props or state that change,
+   * useCallback isn't needed
+   */
+  const handleAddProduct = id => {
     addToCartRequest(id);
   };
 
-  render() {
-    const { products } = this.state;
-    const { amount } = this.props;
+  return (
+    <ProductList>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.formattedPrice}</span>
 
-    return (
-      <ProductList>
-        {products.map(product => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.formattedPrice}</span>
+          <button type="button" onClick={() => handleAddProduct(product.id)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" />{' '}
+              {amount[product.id] || 0}
+            </div>
 
-            <button
-              type="button"
-              onClick={() => this.handleAddProduct(product.id)}
-            >
-              <div>
-                <MdAddShoppingCart size={16} color="#FFF" />{' '}
-                {amount[product.id] || 0}
-              </div>
-
-              <span>ADICIONAR AO CARRINHO</span>
-            </button>
-          </li>
-        ))}
-      </ProductList>
-    );
-  }
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
 
 Home.propTypes = {
